e44811711aad0e6b037aaad43ce2f14a132ba5e2
akiyama <ryo.akiyama112345@gmail.com>
2024-11-30 09:46:43 +0900

    callback処理のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'users#index'
   resources :users, only: %i(index show)
 end
